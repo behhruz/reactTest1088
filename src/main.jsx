ef58bd7f6d0c75c6444747aad22e9efe7c09c473
muhamejanov11 <muhamejanov.n@gmail.com>
2025-02-24 19:39:07 +0500

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Nur from './App.jsx'
+import Nur from './Nur.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
