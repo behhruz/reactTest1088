d17ea50aa8232c981a96a284dae03a85eee1e53d
behhruz <bsatimbayev05@gmail.com>
2025-02-24 19:39:04 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import img from "../src/assets/1.png"
+
+import FirstSection from "./FirstSection"
 
 
 function App() {
@@ -7,26 +8,7 @@ function App() {
 
   return (
     <>
-      <section className="container mx-auto px-6 py-16 flex flex-col md:flex-row items-center">
-        <div className="md:w-1/2 mb-8 md:mb-0">
-          <img src={img} alt="Burj Khalifa" className="rounded-lg shadow-lg" />
-        </div>
-        <div className="md:w-1/2 md:pl-10">
-          <h2 className="text-3xl font-bold mb-4">Вложите в будущее: Недвижимость в Дубае</h2>
-          <p className="mb-4">
-            Дубай, один из самых быстроразвивающихся городов мира, предлагает огромные
-            возможности для инвестиций в недвижимость. Благоприятный налоговый климат, устойчивый рост
-            экономики и высокий уровень жизни делают этот рынок привлекательным для инвесторов со всего
-            мира.
-          </p>
-          <ul className="list-disc pl-6 space-y-2">
-            <li>Отсутствие налогов на доходы от недвижимости</li>
-            <li>Рост возможностей для получения пассивного дохода</li>
-            <li>Стабильная экономика и растущий рынок</li>
-            <li>Стратегическое расположение и инфраструктура</li>
-          </ul>
-        </div>
-      </section>
+      <FirstSection />
 
     </>
   )
